41c32cee5b4f915df175279fa771ea4ef4c06a1c
wangqiang <qiang.wang@seedeer.com>
2019-03-19 17:34:08 +0800

    函数使用

--- function/funTest.go
+++ function/funTest.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x := min(1, 3, 2, 0)
+	fmt.Printf("The minimum is: %d\n", x)
+	arr := []int{7, 9, 3, 5, 1}
+	x = min(arr...)
+	fmt.Printf("The minimum in the array arr is: %d", x)
+}
+
+func min(a ...int) int {
+	if len(a) == 0 {
+		return 0
+	}
+	min := a[0]
+	for _, val := range a {
+		if val < min {
+			min = val
+		}
+	}
+	return min
 }
