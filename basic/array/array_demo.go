609f393643d9880f68015e1419e133987b91cbf3
wangqiang <qiang.wang@seedeer.com>
2019-04-01 14:53:27 +0800

    array

--- basic/array/array_demo.go
+++ basic/array/array_demo.go
@@ -1,14 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	init1()
-	fmt.Println("-----------")
-	init2()
-	fmt.Println("-----------")
-	init3()
-	fmt.Println("-----------")
+	//init1()
+	//fmt.Println("-----------")
+	//init2()
+	//fmt.Println("-----------")
+	//init3()
+	//fmt.Println("-----------")
+	fmt.Println(twoSum([] int{3,2,4}, 6))
 }
 
 func init1() {
@@ -30,4 +33,19 @@ func init3() {
 	for i := range arrKeyValue {
 		fmt.Println("Array item", i, "is", arrKeyValue[i])
 	}
+}
+
+func twoSum(nums []int, target int) []int {
+	for idx1, v1 := range nums {
+		for idx2, v2 := range nums {
+			if idx1!= idx2 && v1 + v2 == target {
+				var idx = make([]int, 2)
+				idx[0] = idx1
+				idx[1] = idx2
+				return idx
+			}
+		}
+	}
+
+	panic("No two sum solution")
 }
